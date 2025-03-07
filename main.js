793a173dc44a351101c7d4a4a769c20bd4ea847d
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-03-06 22:21:45 -0300

    Update files

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ console.log("Electron - Processo Principal")
 // Importação dos recursos do framework
 // app (aplicação)
 // BrowserWindow (Criação da janela)
-const { app, BrowserWindow } = require('electron/main')
+// nativeTheme (definir tema claro ou escuro)
+// Menu (definir um menu personalizado)
+// shell (acessar links externos no navegador padrão)
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
 // Janela principal
 let win
 const createWindow = () => {
+  // definindo tema da janela claro ou escuro
+  nativeTheme.themeSource = 'light'
   win = new BrowserWindow({
+    
     width: 1010,
     height: 720,
     //frame: false,
@@ -18,8 +24,36 @@ const createWindow = () => {
     //autoHideMenuBar: true
   })
 
+  // Carregar o menu personalizado
+  // Atenção! Antes importar o recurso Menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   // carregar o documunte html na janela
   win.loadFile('./src/views/index.html')
+}
+
+// Janela sobre
+let about
+function aboutWindow() {
+  nativeTheme.themeSource='light'
+  // Obter a janela prinipal
+const mainWindow = BrowserWindow.getFocusedWindow()
+// validação (se existir a janela principal)
+if (mainWindow) {
+  about = new BrowserWindow({
+    width: 320,
+    height: 280,
+    autoHideMenuBar: true,
+    resizable: false,
+    minimizable: false,
+    // estabelecer uma relação hierarquica entre janelas
+    parent: mainWindow,
+    // criar uma janela modal (só retorna a principal quando encerrada)
+    modal: true
+  })
+}
+  
+  about.loadFile('./src/views/sobre.html')
 }
 
 // inicialização da aplicação (assincronismo)
@@ -40,3 +74,66 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// Reduzir a verbosidade de logs não criticos (devtools)
+app.commandLine.appendSwitch('log-level','3')
+
+// template do menu
+const template = [
+  {
+    label: 'Notas',
+    submenu: [
+      {
+        label: 'Criar nota',
+        accelerator: 'Ctrl+N',
+        click: () => console.log("teste")
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sair',
+        accelerator: 'Alt+F4',
+        click: () => app.quit()
+      }
+    ]
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Ampliar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar o zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'DevTools',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Repositório',
+        click: () => shell.openExternal('https://github.com/Jean-Soares9/stickynotes')
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
+
